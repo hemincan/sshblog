3e2a75e7d986fa0956db23eb255f879659f33ee4
hemincan <1027871354@qq.com>
2018-06-07 12:04:14 +0800

    44

--- src/main/java/com/xrom/ssh/service/impl/UserServiceImpl.java
+++ src/main/java/com/xrom/ssh/service/impl/UserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.xrom.ssh.service.impl;
 
-import com.xrom.ssh.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.xrom.ssh.repository.SysUserRepository;
+import com.xrom.ssh.service.UserService;
+@Service
 public class UserServiceImpl implements UserService {
 
+	@Autowired(required = true)
+	private SysUserRepository userRepository;
+	
 	public void login(String account, String password){
-		
+		userRepository.delete(id)
 	}
 }
